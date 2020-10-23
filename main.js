10c6f54db2572c98c5c72cf4ca60f995ca8efc18
Ellie Brayton <ebrayton@ufl.edu>
2020-10-23 15:34:51 -0500

    Completed assignment

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // It is always helpful to use comments in your code!
+
+var mainLink = document.getElementsByClassName('main-link');
+console.log(mainLink);
+
+function toggleDropdown(){
+    this.children[1].classList.toggle('show');
+ }
+ 
+ console.log(toggleDropdown);
+
+ for(i=0;i<mainLink.length;i++){
+     mainLink[i].addEventListener('mouseenter', toggleDropdown);
+     mainLink[i].addEventListener('mouseleave', toggleDropdown);
+ }
